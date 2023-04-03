d861fcb6d1a9367ced73964627e70a15d2cfaa9a
Joseph Munoz <munoz_joseph@bah.com>
2023-04-02 22:47:27 -0400

    Better resolution message.

--- argautoopts/resolver.py
+++ argautoopts/resolver.py
@@ -87,8 +87,9 @@ class IOCResolverType:
             raise ResolveException(f'Type was unexpected and must be registered before ' \
                 'attempting resolution.')
         if reg_name not in self._registered:
-            raise ResolveException(f'<{reg_name}> was decorated, but args must be ' \
-                'given to a frontend before requesting objects.')
+            raise ResolveException('Arguments must be supplied to front-end for registered ' \
+                f'type <{reg_name}>. Verify that arguments are correctly passed to a front-end ' \
+                'before resolving.')
 
         # Inflate a class with args
         inflate_args = self._registered[reg_name].copy()
